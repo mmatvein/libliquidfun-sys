1ca7326f1e0b1ed92cfcfd067003f3813d3e6865
Mikael Matveinen <mikael@lingongames.com>
2023-10-28 22:20:23 +0300

    Add factory methods for rest of joint types

--- include/extras.hpp
+++ include/extras.hpp
@@ -83,7 +83,6 @@ extern "C" {
             float maxMotorTorque,
             float motorSpeed) {
         b2RevoluteJointDef def;
-        def.type = b2JointType::e_revoluteJoint;
         def.bodyA = bodyA,
         def.bodyB = bodyB,
         def.collideConnected = collideConnected;
@@ -116,7 +115,6 @@ extern "C" {
             float maxMotorForce,
             float motorSpeed) {
         b2PrismaticJointDef def;
-        def.type = b2JointType::e_prismaticJoint;
         def.bodyA = bodyA,
         def.bodyB = bodyB,
         def.collideConnected = collideConnected;
@@ -133,36 +131,200 @@ extern "C" {
         return static_cast<b2PrismaticJoint*>(world.CreateJoint(&def));
     }
 
-    b2DistanceJoint* CreateDistanceJoint(b2World& world, const b2DistanceJointDef* def) {
-        return static_cast<b2DistanceJoint*>(world.CreateJoint(def));
+    b2DistanceJoint* CreateDistanceJoint(
+        b2World& world,
+        b2Body* bodyA,
+        b2Body* bodyB,
+        bool collideConnected,
+		b2Vec2 localAnchorA,
+		b2Vec2 localAnchorB,
+		float length,
+		float minLength,
+        float maxLength,
+		float stiffness,
+        float damping
+     ) {
+        b2DistanceJointDef def;
+        def.bodyA = bodyA;
+        def.bodyB = bodyB;
+        def.collideConnected = collideConnected;
+		def.localAnchorA = localAnchorA;
+		def.localAnchorB = localAnchorB;
+		def.length = length;
+		def.minLength = minLength;
+		def.maxLength = maxLength;
+		def.stiffness = stiffness;
+		def.damping = damping;
+        return static_cast<b2DistanceJoint*>(world.CreateJoint(&def));
     }
 
-    b2PulleyJoint* CreatePulleyJoint(b2World& world, const b2PulleyJointDef* def) {
-        return static_cast<b2PulleyJoint*>(world.CreateJoint(def));
+    b2PulleyJoint* CreatePulleyJoint(
+        b2World& world,
+        b2Body* bodyA,
+        b2Body* bodyB,
+        bool collideConnected,
+		b2Vec2 groundAnchorA,
+		b2Vec2 groundAnchorB,
+		b2Vec2 localAnchorA,
+		b2Vec2 localAnchorB,
+		float lengthA,
+		float lengthB,
+		float ratio
+    ) {
+        b2PulleyJointDef def;
+        def.bodyA = bodyA;
+        def.bodyB = bodyB;
+        def.collideConnected = collideConnected;
+		def.groundAnchorA = groundAnchorA;
+		def.groundAnchorB = groundAnchorB;
+		def.localAnchorA = localAnchorA;
+		def.localAnchorB = localAnchorB;
+		def.lengthA = lengthA;
+		def.lengthB = lengthB;
+		def.ratio = ratio;
+        return static_cast<b2PulleyJoint*>(world.CreateJoint(&def));
     }
 
-    b2MouseJoint* CreateMouseJoint(b2World& world, const b2MouseJointDef* def) {
-        return static_cast<b2MouseJoint*>(world.CreateJoint(def));
+    b2MouseJoint* CreateMouseJoint(
+        b2World& world,
+        b2Body* bodyA,
+        b2Body* bodyB,
+        bool collideConnected,
+		b2Vec2 target,
+		float maxForce,
+		float stiffness,
+		float damping
+    ) {
+        b2MouseJointDef def;
+        def.bodyA = bodyA;
+        def.bodyB = bodyB;
+        def.collideConnected = collideConnected;
+		def.target = target;
+		def.maxForce = maxForce;
+		def.stiffness = stiffness;
+		def.damping = damping;
+        return static_cast<b2MouseJoint*>(world.CreateJoint(&def));
     }
 
-    b2GearJoint* CreateGearJoint(b2World& world, const b2GearJointDef* def) {
-        return static_cast<b2GearJoint*>(world.CreateJoint(def));
+    b2GearJoint* CreateGearJoint(
+        b2World& world,
+        b2Body* bodyA,
+        b2Body* bodyB,
+        bool collideConnected,
+		b2Joint* joint1,
+		b2Joint* joint2,
+		float ratio
+    ) {
+        b2GearJointDef def;
+        def.bodyA = bodyA;
+        def.bodyB = bodyB;
+        def.collideConnected = collideConnected;
+		def.joint1 = joint1;
+		def.joint2 = joint2;
+		def.ratio = ratio;
+        return static_cast<b2GearJoint*>(world.CreateJoint(&def));
     }
 
-    b2WheelJoint* CreateWheelJoint(b2World& world, const b2WheelJointDef* def) {
-        return static_cast<b2WheelJoint*>(world.CreateJoint(def));
+    b2WheelJoint* CreateWheelJoint(
+        b2World& world,
+        b2Body* bodyA,
+        b2Body* bodyB,
+        bool collideConnected,
+		b2Vec2 localAnchorA,
+		b2Vec2 localAnchorB,
+		b2Vec2 localAxisA,
+		bool enableLimit,
+		float lowerTranslation,
+		float upperTranslation,
+		bool enableMotor,
+		float maxMotorTorque,
+		float motorSpeed,
+		float stiffness,
+		float damping
+    ) {
+        b2WheelJointDef def;
+        def.bodyA = bodyA;
+        def.bodyB = bodyB;
+        def.collideConnected = collideConnected;
+		def.localAnchorA = localAnchorA;
+		def.localAnchorB = localAnchorB;
+		def.localAxisA = localAxisA;
+		def.enableLimit = enableLimit;
+		def.lowerTranslation = lowerTranslation;
+		def.upperTranslation = upperTranslation;
+		def.enableMotor = enableMotor;
+		def.maxMotorTorque = maxMotorTorque;
+		def.motorSpeed = motorSpeed;
+		def.stiffness = stiffness;
+		def.damping = damping;
+        return static_cast<b2WheelJoint*>(world.CreateJoint(&def));
     }
 
-    b2WeldJoint* CreateWeldJoint(b2World& world, const b2WeldJointDef* def) {
-        return static_cast<b2WeldJoint*>(world.CreateJoint(def));
+    b2WeldJoint* CreateWeldJoint(
+        b2World& world,
+        b2Body* bodyA,
+        b2Body* bodyB,
+        bool collideConnected,
+		b2Vec2 localAnchorA,
+		b2Vec2 localAnchorB,
+		float referenceAngle,
+		float stiffness,
+		float damping
+    ) {
+        b2WeldJointDef def;
+        def.bodyA = bodyA;
+        def.bodyB = bodyB;
+        def.collideConnected = collideConnected;
+		def.localAnchorA = localAnchorA;
+		def.localAnchorB = localAnchorB;
+		def.referenceAngle = referenceAngle;
+		def.stiffness = stiffness;
+		def.damping = damping;
+        return static_cast<b2WeldJoint*>(world.CreateJoint(&def));
     }
 
-    b2FrictionJoint* CreateFrictionJoint(b2World& world, const b2FrictionJointDef* def) {
-        return static_cast<b2FrictionJoint*>(world.CreateJoint(def));
+    b2FrictionJoint* CreateFrictionJoint(
+        b2World& world,
+        b2Body* bodyA,
+        b2Body* bodyB,
+        bool collideConnected,
+        b2Vec2 localAnchorA,
+		b2Vec2 localAnchorB,
+		float maxForce,
+		float maxTorque
+    ) {
+        b2FrictionJointDef def;
+        def.bodyA = bodyA;
+        def.bodyB = bodyB;
+        def.collideConnected = collideConnected;
+        def.localAnchorA = localAnchorA;
+		def.localAnchorB = localAnchorB;
+		def.maxForce = maxForce;
+		def.maxTorque = maxTorque;
+        return static_cast<b2FrictionJoint*>(world.CreateJoint(&def));
     }
 
-    b2MotorJoint* CreateMotorJoint(b2World& world, const b2MotorJointDef* def) {
-        return static_cast<b2MotorJoint*>(world.CreateJoint(def));
+    b2MotorJoint* CreateMotorJoint(
+        b2World& world,
+        b2Body* bodyA,
+        b2Body* bodyB,
+        bool collideConnected,
+		b2Vec2 linearOffset,
+		float angularOffset,
+		float maxForce,
+		float maxTorque,
+		float correctionFactor
+    ) {
+        b2MotorJointDef def;
+        def.bodyA = bodyA;
+        def.bodyB = bodyB;
+        def.collideConnected = collideConnected;
+		def.linearOffset = linearOffset;
+		def.angularOffset = angularOffset;
+		def.maxForce = maxForce;
+		def.maxTorque = maxTorque;
+		def.correctionFactor = correctionFactor;
+        return static_cast<b2MotorJoint*>(world.CreateJoint(&def));
     }
 }
 
